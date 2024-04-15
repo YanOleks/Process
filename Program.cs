eae658db4eb18af50d6738c13a7965e879a77546
YanOleks <yanytskyi.oleksandr@gmail.com>
2024-04-15 12:20:11 +0300

    Reading from file by line. To binary. Mov

--- Program.cs
+++ Program.cs
@@ -1,10 +1,116 @@
-﻿namespace Process
+﻿using System.Collections;
+using System.IO;
+
+namespace Process
 {
     internal class Program
     {
+        #region Constants
+        const String FileLocation = "../../../Commands.txt";
+        const int Size = 26;
+        #endregion
+
+        #region Technical
+        enum Register { R1, R2 };
+        static List<String[]> buffer = [];
+        #endregion
+
+        #region Registers
+        static BitArray[] registers = [
+            new(Size, false),
+            new(Size, false),
+        ];      
+        static bool PS = false;
+        static int PC = 0;
+        static int TC = 0;
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MOV(Register.R1, 13);
+            PrintBinaryArray(registers[(int)Register.R1]);
+            
         }
+        #region Technical Commands
+        static void PrintBinaryArray(IEnumerable<bool> array)
+        {
+            foreach(var i in array)
+            {
+                Console.Write(i ? "1":"0");
+            }
+        }
+        static void PrintBinaryArray(BitArray array)
+        {
+            int j = 1;
+            foreach (bool i in array)
+            {
+                if (j % 4 == 0) Console.Write(" ");
+                Console.Write(i ? "1" : "0");
+                j++;
+
+            }
+        }
+        static void ReadCommandFile(string path)
+        {
+            String? line;
+            try
+            {
+                StreamReader sr = new(path);
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    buffer.Add(ReadCommand(line));
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();                    
+                }
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+        static String[] ReadCommand(string command)
+        {
+            return command.Split(' ');
+        }
+        static List<bool> ToBinary(int value)
+        {
+            List<bool> result = [];
+            while(value > 0)
+            {
+                result.Add(value % 2 == 1);
+                value /= 2;
+            }
+            result.Reverse();
+            return result;
+        }
+        static void Execute()
+        {
+#warning Not implemented
+        }
+        #endregion
+
+        #region Commands
+        //plave value into register
+        static void MOV(Register writeInto, int value)
+        {
+            var array = ToBinary(value);
+            for(int i = 1; i <= array.Count; i++)
+            {
+                registers[(int)writeInto][^i] = array[^i];
+            }
+        }
+        static void MOV(Register writeInto, Register writeOut)
+        {
+            registers[(int)writeInto] = registers[(int)writeOut];
+        }
+        
+        #endregion
     }
 }
